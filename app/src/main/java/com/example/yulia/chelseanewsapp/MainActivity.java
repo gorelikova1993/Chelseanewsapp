6244e64878bec126c272d67e063f50271f94afa8
Yulia Gorelikova <julia.gorelikova@OCRV.RU>
2018-08-08 10:22:33 +0300

    5

--- app/src/main/java/com/example/yulia/chelseanewsapp/MainActivity.java
+++ app/src/main/java/com/example/yulia/chelseanewsapp/MainActivity.java
@@ -10,6 +10,8 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
@@ -70,6 +72,25 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
             empryTextView.setText(R.string.no_internet_connection);
         }
 
+    }
+
+    @Override
+    // This method initialize the contents of the Activity's options menu.
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the Options Menu we specified in XML
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            Intent settingsIntent = new Intent(this, SettingsActivity.class);
+            startActivity(settingsIntent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     public Loader<List<Item>> onCreateLoader(int i, Bundle bundle) {
